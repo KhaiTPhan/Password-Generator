b6094d956cc7a783cd6b65571902aaa32107ced4
King of Chu <khai.phan.2007@gmail.com>
2021-02-05 22:47:32 +1100

    updating files

--- script.js
+++ script.js
@@ -1,34 +1,85 @@
-// generate random password
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-function generate() {
+// Write password to the #password input
+function writePassword() {
 
-//set password length
-var passwordLength = document.getElementById("slider").value;
+// Setting the password length
 
+  var passwordLength = prompt("Please nominate the length of your password by inputting a number between 8 and 128.");
 
-//possible password values
+  // Determining the possible password values
+
+  var lowercaseCheck = confirm("Do you want to include lowercases in the password?");
+  var uppercaseCheck = confirm("Do you want to include uppercases in the password?");
+  var numericCheck = confirm("Do you want to include numbers in the password?");
+  var specialCharCheck = confirm("Do you want to include special characters in the password?");
+
+  var lowercase = "abcdefghijklmnopqrstuvwxyz"
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var numeric = "123456789"
+  var specialChar = "!@#$%^&*_+"
+ 
+  if (lowercaseCheck === true && uppercaseCheck === true && numericCheck === true && specialCharCheck === true) {
+    var values = lowercase + uppercase + numeric + specialChar;
+    } 
+  else if (lowercaseCheck !== true && uppercaseCheck === true && numericCheck === true && specialCharCheck === true) {
+    var values = uppercase + numeric + specialChar;
+    } 
+  else if (lowercaseCheck !== true && uppercaseCheck !== true && numericCheck === true && specialCharCheck === true) {
+    var values = numeric + specialChar;
+  } 
+  else if (lowercaseCheck !== true && uppercaseCheck !== true && numericCheck !== true && specialCharCheck === true) {
+    var values = specialChar;
+  }
+  else if (lowercaseCheck === true && uppercaseCheck !== true && numericCheck !== true && specialCharCheck === true) {
+    var values = lowercase + specialChar;
+  }
+  else if (lowercaseCheck !== true && uppercaseCheck === true && numericCheck !== true && specialCharCheck === true) {
+    var values = uppercase + specialChar;
+  }
+  else if (lowercaseCheck === true && uppercaseCheck !== true && numericCheck === true && specialCharCheck === true) {
+    var values = lowercase + numeric + specialChar;
+  }
+  else if (lowercaseCheck === true && uppercaseCheck === true && numericCheck !== true && specialCharCheck === true) {
+    var values = lowercase + uppercase + specialChar;
+  }
+  else if (lowercaseCheck === true && uppercaseCheck === true && numericCheck === true && specialCharCheck !== true) {
+    var values = lowercase + uppercase + numeric;
+  }
+  else if (lowercaseCheck === true && uppercaseCheck === true && numericCheck !== true && specialCharCheck !== true) {
+    var values = lowercase + uppercase;
+  }
+  else if (lowercaseCheck === true && uppercaseCheck !== true && numericCheck !== true && specialCharCheck !== true) {
+    var values = lowercase;
+  }
+  else if (lowercaseCheck === true && uppercaseCheck !== true && numericCheck === true && specialCharCheck !== true) {
+    var values = lowercase + numeric;
+  }
+  else if (lowercaseCheck !== true && uppercaseCheck === true && numericCheck === true && specialCharCheck !== true) {
+    var values = uppercase + numeric;
+  }
+  else if (lowercaseCheck !== true && uppercaseCheck === true && numericCheck !== true && specialCharCheck !== true) {
+    var values = uppercase;
+  }
+  else {
+    var values = numeric;
+  } 
   
-var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*_+";
 
+  
   var password = "";
 
 //create for loop to choose password characters
-for(var i = 0; i <= passwordLength; i++) {
+  for(var i = 0; i <= passwordLength; i++) {
   password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-
 }
 
+  var passwordText = document.querySelector("#password");
 
-//add password to textbox/display area
-document.getElementById("password").value = password;  
+  passwordText.value = password;
 
-}
+};
 
-//set default length display of 16
-document.getElementById("length").innerHTML = "length: 16";
-
-//function to set length based on slider position
-document.getElementById("slider").oninput = function(){
-
-    document.getElementById("length").innerHTML = "Length: " + document.getElementById("slider").value;
-}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
